9871a56981c2219192a1412656a410664a183a98
Marcio Sousa <marciofrapeso@gmail.com>
2020-11-07 22:38:53 -0300

    ajustando tamanho da imagem

--- src/components/Cartao/index.js
+++ src/components/Cartao/index.js
@@ -10,42 +10,42 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: '100%',
-  },
-  media: {
-    height: 500,
-  },
+    root: {
+        maxWidth: '100%'
+    },
+    media: {
+        height: 180
+    }
 });
 
 export default function MediaCard() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image='http://source.unsplash.com/featured/dog'
-          title='Seja bem Vindo'
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Nossa Missão
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image='http://source.unsplash.com/featured/dog'
+                    title='Seja bem Vindo'
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Nossa Missão
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            A missão da Delta é com o ....
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        A missão da Delta é com o ....
           </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Compartilhe
-        </Button>
-        <Button size="small" color="primary">
-          Saiba Mais
-        </Button>
-      </CardActions>
-    </Card>
-  );
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Compartilhe
+                    </Button>
+                <Button size="small" color="primary">
+                    Saiba Mais
+                    </Button>
+            </CardActions>
+        </Card>
+    );
 }
